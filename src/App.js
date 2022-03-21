04a09fbe3d3c7e1186df05002795f4ae26d0cd01
Mickael-DP <m.dallepasqualine@gmail.com>
2022-03-21 15:48:08 +0100

    Add header footer

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
-import {React, useEffect, useState} from 'react';
+import {React} from 'react';
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import './App.css';
 import Home from './home/Home';
+import Header from "./comons/header/Header";
+import Footer from "./comons/footer/Footer";
 
 export default function  App() {
   
   return (
     <>
       <BrowserRouter>
+          <Header/>
       <Routes>
        <Route path="/" element={<Home />} />
+
       </Routes>
+        <Footer/>
       </BrowserRouter>
     </>
   );
