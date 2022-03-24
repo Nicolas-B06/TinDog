35b3729da6d947a979ada45213c8871f4ea462b7
Mickael-DP <m.dallepasqualine@gmail.com>
2022-03-24 16:08:31 +0100

    bug filtre

--- src/breeds/Breeds.js
+++ src/breeds/Breeds.js
@@ -1,7 +1,45 @@
-
-
+import { React, useEffect, useState } from "react";
+import { FormControl, InputLabel, Select } from "@mui/material";
+import MenuItem from "@mui/material/MenuItem";
+// https://api.TheDogAPI.com/v1/breeds
 export default function Breeds() {
+  const [dataBreeds, setDataBreeds] = useState();
+  const [nbImg, setNbImg] = useState();
+
+  const breedsUrl = "https://api.TheDogAPI.com/v1/breeds";
+  const apiKey = "8120bc3b-db3b-4675";
+  useEffect(() => {
+    getBreeds();
+  }, []);
+
+  const getBreeds = async () => {
+    try {
+      const response = await fetch(breedsUrl, {
+        method: "GET",
+        headers: { "x-api-key": apiKey },
+      });
+      const data = await response.json();
+      setDataBreeds(data);
+    } catch (err) {
+      console.log(err.stack);
+    }
+  };
+  console.log(dataBreeds);
+  function onChange(e) {
+    setDataBreeds(e.target.value);
+  }
+
   return (
-    <div>Breeds</div>
-  )
+    <FormControl sx={{ m: 1, minWidth: 120 }}>
+      <InputLabel>Breeds</InputLabel>
+      <Select onChange={onChange} onBlur={onChange}>
+        {dataBreeds &&
+          dataBreeds.map((breeds) => {
+            <MenuItem key={breeds.id} value={breeds.name}>
+              {breeds.name}
+            </MenuItem>;
+          })}
+      </Select>
+    </FormControl>
+  );
 }
